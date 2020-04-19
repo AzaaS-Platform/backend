c5467c97c168afc5802e5fed1025d0d6b09b954d
qazedcwsx4 <qazedcwsx4@gmail.com>
2020-04-19 11:34:20 +0200

    AP-20: Implementacja przykładowych endpointów zwracających grupę i użytkownika

--- src/handler.ts
+++ src/handler.ts
@@ -1,28 +1,43 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import {APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda';
 import 'source-map-support/register';
+import {UserService} from "./service/UserService";
+import DatabaseAccessor from "./DatabaseAccessor";
+import {GroupService} from "./service/GroupService";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+function buildResponse(code: number, message: string): APIGatewayProxyResult {
     return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Hello world!',
-            },
-            null,
-            2,
-        ),
+        statusCode: code,
+        body: message
     };
+}
+
+export const hello: APIGatewayProxyHandler = async (_, _context): Promise<APIGatewayProxyResult> => {
+    return buildResponse(200, 'Hello world!')
 };
 
-export const bye: APIGatewayProxyHandler = async (event, _context) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Bye!',
-            },
-            null,
-            2,
-        ),
-    };
+export const bye: APIGatewayProxyHandler = async (_, _context): Promise<APIGatewayProxyResult> => {
+    return buildResponse(200, 'Bye!')
+};
+
+export const getUser: APIGatewayProxyHandler = async (event, _context): Promise<APIGatewayProxyResult> => {
+    const databaseAccessor = new DatabaseAccessor();
+    const groupService = new GroupService(databaseAccessor);
+    const userService = new UserService(databaseAccessor, groupService);
+
+    if (event.queryStringParameters != null) {
+        const user = await userService.getUserByKey(event.queryStringParameters['client'], event.queryStringParameters['id']);
+        console.log(user);
+        return buildResponse(200, JSON.stringify(user));
+    } else return buildResponse(400, JSON.stringify(event));
+};
+
+export const getGroup: APIGatewayProxyHandler = async (event, _context): Promise<APIGatewayProxyResult> => {
+    const databaseAccessor = new DatabaseAccessor();
+    const groupService = new GroupService(databaseAccessor);
+
+    if (event.queryStringParameters != null) {
+        const group = await groupService.getGroupByKey(event.queryStringParameters['client'], event.queryStringParameters['id']);
+        console.log(group);
+        return buildResponse(200, JSON.stringify(group));
+    } else return buildResponse(400, JSON.stringify(event));
 };
