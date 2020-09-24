b8387b46bf89ec38616c682212104c09f60e9427
Maciej <maciejstanuch.ms@gmail.com>
2020-09-24 17:21:57 +0200

    AP-41: Usunięcie problemu z wyciekaniem loginów użytkowników.

--- src/Utils/PermissionsUtils.ts
+++ src/Utils/PermissionsUtils.ts
@@ -1,11 +1,13 @@
 import { HttpError } from '../error/HttpError';
 import * as jwt from 'jsonwebtoken';
 import { JWTPayloadFactory } from '../model/factory/JWTPayloadFactory';
-import { BadRequest } from '../error/BadRequest';
 import { UserService } from '../service/UserService';
 import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
 export class PermissionsUtils {
+    private static INVALID_JSON_WEB_TOKEN = 'Invalid Json Web Token. Authorization not given.';
+    private static TOKEN_EXPIRED = 'Expired Json Web Token. Authorization not given.';
+
     /**
      * Checks for admin permissions and then calls the callback function.
      * @param jwt JWT object
@@ -23,18 +25,23 @@ export class PermissionsUtils {
         return callback();
     }
 
+    /**
+     * Unfortunately it's basically just a copy of AuthenticationService.authorize method.
+     */
     private static async checkAdminPermissions(token: string, userService: UserService): Promise<boolean> {
         try {
             const payload = JWTPayloadFactory.fromToken(token);
             const user = await userService.getByKey(payload.clt, payload.usr);
             if (user === null) {
-                throw new BadRequest('User not found.');
+                throw new JsonWebTokenError(this.INVALID_JSON_WEB_TOKEN);
             }
             jwt.verify(token, user.passwordHash);
             return user.isAdmin;
         } catch (error) {
-            if (error instanceof JsonWebTokenError || error instanceof TokenExpiredError) {
-                throw new HttpError(403, 'Invalid Json Web Token. Authorization not given.');
+            if (error instanceof TokenExpiredError) {
+                throw new HttpError(403, this.TOKEN_EXPIRED);
+            } else if (error instanceof JsonWebTokenError) {
+                throw new HttpError(403, this.INVALID_JSON_WEB_TOKEN);
             }
             throw error;
         }
--- src/service/AuthenticationService.ts
+++ src/service/AuthenticationService.ts
@@ -7,19 +7,16 @@ import { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 import { HttpError } from '../error/HttpError';
 
 export class AuthenticationService {
-    private USER_NOT_FOUND_ERROR = 'User not found.';
     private INCORRECT_CREDENTIALS_ERROR = 'Incorrect credentials';
     private INVALID_JSON_WEB_TOKEN = 'Invalid Json Web Token. Authorization not given.';
+    private TOKEN_EXPIRED = 'Expired Json Web Token. Authorization not given.';
     private TOKEN_EXPIRATION_TIME = '30m';
 
     constructor(private userService: UserService) {}
 
     public async generateTokenForUser(client: string, username: string, password: string): Promise<string> {
         const user = await this.userService.getByUsername(client, username);
-        if (user === null) {
-            throw new BadRequest(this.USER_NOT_FOUND_ERROR);
-        }
-        if (!PasswordUtils.validate(password, user.passwordHash)) {
+        if (user === null || !PasswordUtils.validate(password, user.passwordHash)) {
             throw new BadRequest(this.INCORRECT_CREDENTIALS_ERROR);
         }
         const payload = JWTPayloadFactory.from(client, user.entity);
@@ -28,19 +25,26 @@ export class AuthenticationService {
         return jwt.sign({ payload }, user.passwordHash, { expiresIn: this.TOKEN_EXPIRATION_TIME });
     }
 
-    public async authorizeUser(client: string, token: string, permissionRequired: Array<string>): Promise<boolean> {
+    public async checkPermissionsForUser(
+        client: string,
+        token: string,
+        permissionRequired: Array<string>,
+    ): Promise<boolean> {
         try {
             const payload = JWTPayloadFactory.fromToken(token);
             const user = await this.userService.getByKey(client, payload.usr);
             if (user === null) {
-                throw new BadRequest(this.USER_NOT_FOUND_ERROR);
+                throw new JsonWebTokenError(this.INVALID_JSON_WEB_TOKEN);
             }
             jwt.verify(token, user.passwordHash);
+            //TODO: permissions matcher.
             return permissionRequired.every(permission =>
                 user.groupObjects.flatMap(group => group.permissions).includes(permission),
             );
         } catch (error) {
-            if (error instanceof JsonWebTokenError || error instanceof TokenExpiredError) {
+            if (error instanceof TokenExpiredError) {
+                throw new HttpError(403, this.TOKEN_EXPIRED);
+            } else if (error instanceof JsonWebTokenError) {
                 throw new HttpError(403, this.INVALID_JSON_WEB_TOKEN);
             }
             throw error;
