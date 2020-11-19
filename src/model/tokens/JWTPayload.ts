f337126031fc2c6bd7c689f3458daea58b5cdc39
Maciej Stanuch <maciejstanuch.ms@gmail.com>
2020-11-19 21:05:48 +0100

    AP-59: Dodanie pola `adm` do payloadu JWT.

--- src/model/tokens/JWTPayload.ts
+++ src/model/tokens/JWTPayload.ts
@@ -1,9 +1,11 @@
 export class JWTPayload {
     clt: string;
     usr: string;
+    adm: boolean;
 
-    constructor(client: string, user: string) {
+    constructor(client: string, user: string, isAdmin: boolean) {
         this.clt = client;
         this.usr = user;
+        this.adm = isAdmin;
     }
 }
